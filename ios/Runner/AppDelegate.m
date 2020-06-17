3e76abccb769e57d201d7db2f2a90533e31bd7ab
Manish Tuladhar <manish.eclipse@gmail.com>
2020-06-17 17:06:39 +0545

    Trying native code by showing battery percentage

---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,49 @@
+#import "AppDelegate.h"
+#import "GeneratedPluginRegistrant.h"
+#import <Flutter/Flutter.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+  FlutterMethodChannel* batteryChannel = [FlutterMethodChannel methodChannelWithName:@"samples.flutter.dev/battery" binaryMessenger:controller];
+
+  __weak typeof(self) weakSelf = self;
+
+ [batteryChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result){
+    if([@"getBatteryLevel" isEqualToString:call.method]){
+    int batteryLevel = [weakSelf getBatteryLevel];
+
+    if(batteryLevel == -1){
+        result([FlutterError errorWithCode:@"UNAVAILABLE"
+                             message: @"Battery info not available"
+                             details:nil]);
+    }
+    else{
+    result(@(batteryLevel));
+    }
+ }
+ else{
+    result(FlutterMethodNotImplemented);
+ }
+ }];
+
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (int)getBatteryLevel{
+    UIDevice* device = UIDevice.currentDevice;
+    device.batteryMonitoringEnabled = YES;
+    if(device.batteryState == UIDeviceBatteryStateUnknown){
+        return -1;
+    }
+    else{
+    return (int){device.batteryLevel *100);
+    }
+}
+
+@end
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Native code',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _batteryLevel;
+
+  //get battery level from ios or android platform
+  Future<void> _getBatteryLevel() async {
+    const platform =
+        MethodChannel("samples.flutter.dev/battery"); //unique domain
+    try {
+      final batteryLevel = await platform.invokeMethod('getBatteryLevel');
+      setState(() {
+        _batteryLevel = batteryLevel;
+      });
+    } on PlatformException catch (error) {
+      setState(() {
+        _batteryLevel = null;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getBatteryLevel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Battery"),
+      ),
+      body: Center(
+        child: Text("Battery Level $_batteryLevel"),
+      ),
+    );
+  }
+}
